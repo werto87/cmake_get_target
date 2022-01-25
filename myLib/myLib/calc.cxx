a5a974a32a86ab95c1b48f57b7ae7043fcbdfe3f
werto87 <huhulasmichinruhe@web.de>
2022-01-25 17:37:42 +0100

    initial commit

---
+++ myLib/myLib/calc.cxx
@@ -0,0 +1,3 @@
+#include "../myLib/myLib/calc.hxx"
+
+int calc(int a, int b) { return a + b; }
---
+++ use_my_lib_in_another_project/main.cxx
@@ -0,0 +1,5 @@
+// #include "../myLib/myLib/calc.hxx"
+#include <iostream>
+int main() {
+  //  std::cout << "project using the lib:" << calc(3, 4) << std::endl;
+}
